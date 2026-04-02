ddb2a652d01867e538c24cdff2af1340dc17038d
Eldar Tengizov <eldar.teng@gmail.com>
2026-04-02 23:42:00 +0300

    Add auto-generated CSS module types for FAQPage

---
+++ frontend/src/pages/Legal/FAQPage.module.css.d.ts
@@ -0,0 +1,22 @@
+declare const styles: {
+  readonly "faqAnswer": string;
+  readonly "faqAnswerOpen": string;
+  readonly "faqChevron": string;
+  readonly "faqChevronOpen": string;
+  readonly "faqContent": string;
+  readonly "faqCta": string;
+  readonly "faqCtaButton": string;
+  readonly "faqCtaText": string;
+  readonly "faqCtaTitle": string;
+  readonly "faqIntro": string;
+  readonly "faqItem": string;
+  readonly "faqItemOpen": string;
+  readonly "faqList": string;
+  readonly "faqMain": string;
+  readonly "faqPage": string;
+  readonly "faqPage__container": string;
+  readonly "faqQuestion": string;
+  readonly "faqTitle": string;
+};
+export = styles;
+
